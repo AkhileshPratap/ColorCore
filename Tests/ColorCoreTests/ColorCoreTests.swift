50233531b24430a509cbf8df89398f0b99cc0bb7
Akhilesh Pratap Singh <apsingh@dminc.com>
2020-07-28 22:27:51 +0530

    Documented and updated first public api

---
+++ Tests/ColorCoreTests/ColorCoreTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import ColorCore
+
+final class ColorCoreTests: XCTestCase {
+  
+  func testColorRedEqual() {
+    let color = ColorCore.colorFromHexString("FF0000")
+    XCTAssertEqual(color, .red)
+  }
+  
+  static var allTests = [
+    ("testColorRedEqual", testColorRedEqual)
+  ]
+}
